bd5199b4341bee132590a8a6e7416189e9ecfca7
Anastasia Charushina <stasia-a@tut.by>
2017-08-15 21:34:11 +0300

    Add basic logic to be able to find rank of one hand

---
+++ poker_hand.rb
@@ -0,0 +1,106 @@
+class Card
+  attr_reader :suit, :value, :digit_value
+
+  CONVERT_VALUES = { '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'T' => 10, 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14 }
+
+  def initialize(suit:, value:)
+    @suit = suit
+    @value = value
+    @digit_value = CONVERT_VALUES[value.to_s]
+  end
+
+  def rank
+    value
+  end
+
+  def to_s
+    puts "#{value}#{suit}"
+  end
+end
+
+class Hand
+  attr_reader :cards
+
+  def initialize(cards:)
+    @cards = cards
+  end
+
+  def has_same_suits?(num)
+    group_by_suit.key(num)
+  end
+
+  def consecutive_values?
+    sorted_values.each_cons(2).all? {|a, b| b == a + 1 }
+  end
+
+  def highest_card
+    cards.sort_by { |card| card.digit_value }.last
+  end
+
+  def winning_category
+    @winning_category ||= CategoryCollection.new(self).winning_category
+  end
+
+  def rank
+    winning_category.rank
+  end
+
+  private
+
+  def pluck(attr)
+    cards.map(&attr.to_sym)
+  end
+
+  def sorted_values
+    pluck(:digit_value).sort
+  end
+
+  def group_by_suit
+    cards.inject(Hash.new(0)) { |h, c| h[[c.suit]] += 1 ; h }
+  end
+
+  def group_by_value
+    cards.inject(Hash.new(0)) { |h, c| h[[c.value]] += 1 ; h }
+  end
+end
+
+class Category
+  attr_reader :hand
+
+  def initialize(hand)
+    @hand = hand
+  end
+
+  def rank
+    hand.highest_card.rank
+  end
+end
+
+class StraightFlush < Category
+
+  def satisfy
+    hand.has_same_suits?(5) && hand.consecutive_values?
+  end
+end
+
+class HighCard < Category
+  def satisfy
+    true
+  end
+end
+
+class CategoryCollection
+  attr_reader :hand
+
+  CATEGORY_ORDER = [:straight_flush, :high_card]
+
+  def initialize(hand)
+    @hand = hand
+  end
+
+  def winning_category
+    CATEGORY_ORDER.map { |name| name.to_s.classify.constantize.send(:new, hand) }.find do |category|
+      category.satisfy
+    end
+  end
+end
